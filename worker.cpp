74bdb200596d2163e9d2940e080c63748dcf3125
Jackie <jmh37b@mail.umsl.edu>
2026-04-07 15:57:24 -0500

    created worker

---
+++ worker.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+
+struct msgbuffer {
+	long mtype;
+	int quantum;
+};
+
+int main(int argc, char** argv) {
+
+	// attach to same message queue
+	int msqid = msgget(IPC_PRIVATE, 0666);
+	if (msqid == -1) {
+		perror("msgget failed");
+		exit(1);
+	}
+
+	pid_t mypid = getpid();
+
+    	// Seed random using PID
+    	srand(mypid);
+
+	while (true) {
+		msgbuffer msg;
+
+		// wait for message from oss
+		if (msgrcv(msqid, &msg, sizeof(int), mypid, 0) == -1) {
+	            perror("msgrcv failed");
+        	    exit(1);
+        	}	
+
+        	int quantum = msg.quantum;
+
+        	int action = rand() % 100;
+
+        	msgbuffer response;
+        	response.mtype = 1;
+		
+		if (action < 20) {
+			int timeUsed = rand() % quantum;
+			response.quantum = timeUsed;
+		}
+
+		else if (action < 40) {
+         	   int timeUsed = rand() % quantum;
+		   response.quantum = -timeUsed;
+		   msgsnd(msqid, &response, sizeof(int), 0);
+		   break;
+        	}
+
+		else {
+			response.quantum = quantum;
+		}
+
+		if (msgsnd(msqid, &response, sizeof(int), 0) == -1) {
+			perror("msgsnd failed");
+			exit(1);
+		}
+	}
+	return 0;
+}
+
