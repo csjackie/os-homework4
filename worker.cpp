f101f9f464b57b3b3f0e90ee5a625ab7b511cb3d
Jackie <jmh37b@mail.umsl.edu>
2026-04-07 16:33:31 -0500

    updates

--- worker.cpp
+++ worker.cpp
@@ -1,4 +1,9 @@
 #include <iostream>
+#include <unistd.h>
+#include <cstdlib>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <ctime>
 
 struct msgbuffer {
 	long mtype;
@@ -8,7 +13,8 @@ struct msgbuffer {
 int main(int argc, char** argv) {
 
 	// attach to same message queue
-	int msqid = msgget(IPC_PRIVATE, 0666);
+	key_t key = ftok(".", 'A');
+	int msqid = msgget(key, 0666);
 	if (msqid == -1) {
 		perror("msgget failed");
 		exit(1);
@@ -29,8 +35,8 @@ int main(int argc, char** argv) {
         	}	
 
         	int quantum = msg.quantum;
-
-        	int action = rand() % 100;
+		int action = rand() % 100;
+		int totalUsed = 0;
 
         	msgbuffer response;
         	response.mtype = 1;
@@ -38,13 +44,17 @@ int main(int argc, char** argv) {
 		if (action < 20) {
 			int timeUsed = rand() % quantum;
 			response.quantum = timeUsed;
+			totalUsed += timeUsed;
 		}
 
 		else if (action < 40) {
-         	   int timeUsed = rand() % quantum;
-		   response.quantum = -timeUsed;
-		   msgsnd(msqid, &response, sizeof(int), 0);
-		   break;
+         	   	int timeUsed = rand() % quantum;
+		   	response.quantum = -timeUsed;
+		   	if (msgsnd(msqid, &response, sizeof(int), 0) == -1) {
+			   	perror("msgsnd failed");
+			   	exit(1);
+		   	}
+		   	break;
         	}
 
 		else {
